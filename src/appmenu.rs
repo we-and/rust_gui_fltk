f7bd53d4a9649f7708b8a5744af30eb86409e940
JD <jbmdumont@gmail.com>
2024-04-01 11:39:48 +0200

    cleanup

---
+++ src/appmenu.rs
@@ -0,0 +1,257 @@
+use fltk::dialog;
+use fltk::{
+    app,
+    button::*,
+    group::{Flex, Tabs},
+    input::Input,
+    menu::{Choice, MenuBar, MenuButton},
+    output::Output,
+    prelude::{GroupExt, InputExt, MenuExt, WidgetBase, WidgetExt, WindowExt},
+    window::Window,
+};
+use fltk::{enums::*, frame::Frame, group::*, menu::*, prelude::*, window::*};
+use fltk::{prelude::*, *};
+
+
+#[derive(Debug, Copy, Clone)]
+pub enum Message {
+    File_CorpusNuovo,
+    File_CorpusEsistente,
+    File_ScanParseAlfabeto,
+    File_ScanParseSeparator,
+    File_Importa_MieRisorse,
+    File_Importa_TabellaInDataset,
+    File_Importa_ListaSempliceInDataset,
+    File_Esporta_DocumentiTipi,
+    File_Esporta_TipiVariabili,
+    File_Esporta_RiconstruzioneCorpus,
+    File_Esporta_Subcorpus,
+    File_Esci,
+    Analisi_Pretratamento,
+    Analisi_Lessicale_Tagging,
+   Analisi_Lessicale_EstrazioneDiParoleChiave,
+    Analisi_Testuale,
+    Impostationi_ModalitaCore_MonoCore,
+    Impostationi_ModalitaCore_MultiCore,
+    Impostationi_ModalitaSchermo,
+    Info_SuTalTac4,
+    Info_Licenza,
+    Info_ManualeUtente,
+    Info_Tutorial,
+    Info_Supporto
+}
+
+
+pub fn build_menu(s: app::Sender<Message>,WIN_WIDTH :i32) {
+  
+
+
+    let mut menu_bar = MenuBar::new(0, 0, WIN_WIDTH, 30, None);
+    menu_bar.add_emit(
+        "&File/Corpus Nuovo\t",
+        Shortcut::None,
+        menu::MenuFlag::Normal,
+        s,
+        Message::File_CorpusNuovo,
+    );
+    menu_bar.add_emit(
+        "&File/Corpus esistente\t",
+        Shortcut::None,
+        menu::MenuFlag::Normal,
+        s,
+        Message::File_CorpusEsistente,
+    );    menu_bar.add_emit(
+        "&File/Scan + Parse Alfabeto\t",
+        Shortcut::None,
+        menu::MenuFlag::Normal,
+        s,
+        Message::File_ScanParseAlfabeto,
+    );    menu_bar.add_emit(
+        "&File/Scan + Parse Separatori\t",
+        Shortcut::None,
+        menu::MenuFlag::Normal,
+        s,
+        Message::File_ScanParseSeparator,
+    );
+
+    menu_bar.add_emit(
+        "&File/Importa/Mie risorse\t",
+        Shortcut::None,
+        menu::MenuFlag::Normal,
+        s,
+        Message::File_Importa_MieRisorse,
+    );
+    menu_bar.add_emit(
+        "&File/Importa/Tabella in Dataset\t",
+        Shortcut::None,
+        menu::MenuFlag::Normal,
+        s,
+        Message::File_Importa_TabellaInDataset,
+    );
+
+    menu_bar.add_emit(
+        "&File/Importa/Lista semplice in Dataset\t",
+        Shortcut::None,
+        menu::MenuFlag::Normal,
+        s,
+        Message::File_Importa_ListaSempliceInDataset,
+    );
+    menu_bar.add_emit(
+        "&File/Esporta/Documenti - tipi",
+        Shortcut::None,
+        menu::MenuFlag::Normal,
+        s,
+        Message::File_Esporta_DocumentiTipi,
+    );
+
+    menu_bar.add_emit(
+        "&File/Esporta/Tipi - Variabili",
+        Shortcut::None,
+        menu::MenuFlag::Normal,
+        s,
+        Message::File_Esporta_TipiVariabili,
+    );
+
+    menu_bar.add_emit(
+        "&File/Esporta/Riconstruzione Corpus",
+        Shortcut::None,
+        menu::MenuFlag::Normal,
+        s,
+        Message::File_Esporta_RiconstruzioneCorpus,
+    );
+
+    menu_bar.add_emit(
+        "&File/Esporta/Subcorpus",
+        Shortcut::None,
+        menu::MenuFlag::Normal,
+        s,
+        Message::File_Esporta_Subcorpus,
+    );
+
+    menu_bar.add_emit(
+        "&File/Esci\t",
+        Shortcut::None,
+        menu::MenuFlag::Normal,
+        s,
+        Message::File_Esci,
+    );
+    menu_bar.add_emit(
+        "Analisi/Pre-Tratamento\t",
+        Shortcut::None,
+        menu::MenuFlag::Normal,
+        s,
+        Message::Analisi_Pretratamento,
+    );
+    menu_bar.add_emit(
+        "Analisi/Lessicale/Tagging\t",
+        Shortcut::None,
+        menu::MenuFlag::Normal,
+        s,
+        Message::Analisi_Lessicale_Tagging,
+    );
+    menu_bar.add_emit(
+        "Analisi/Lessicale/Estrazione di Parole Chiave\t",
+        Shortcut::None,
+        menu::MenuFlag::Normal,
+        s,
+        Message::Analisi_Lessicale_EstrazioneDiParoleChiave,
+    );
+    menu_bar.add_emit(
+        "Analisi/Testuale\t",
+        Shortcut::None,
+        menu::MenuFlag::Normal,
+        s,
+        Message::Analisi_Testuale,
+    );
+
+    menu_bar.add_emit(
+        "Impostationi/Modalità Core/Mono-Core\t",
+        Shortcut::None,
+        menu::MenuFlag::Normal,
+        s,
+        Message::Impostationi_ModalitaCore_MonoCore,
+    );
+    menu_bar.add_emit(
+        "Impostationi/Modalità Core/Multi-Core\t",
+        Shortcut::None,
+        menu::MenuFlag::Normal,
+        s,
+        Message::Impostationi_ModalitaCore_MultiCore,
+    );
+    menu_bar.add_emit(
+        "Impostationi/Modalità Schermo\t",
+        Shortcut::None,
+        menu::MenuFlag::Normal,
+        s,
+        Message::Impostationi_ModalitaSchermo,
+    );
+    menu_bar.add_emit(
+        "Info/Su TalTac4\t",
+        Shortcut::None,
+        menu::MenuFlag::Normal,
+        s,
+        Message::Info_SuTalTac4,
+    );
+    menu_bar.add_emit(
+        "Info/Licenza\t",
+        Shortcut::None,
+        menu::MenuFlag::Normal,
+        s,
+        Message::Info_Licenza,
+    );
+    menu_bar.add_emit(
+        "Info/Manuale Utente\t",
+        Shortcut::None,
+        menu::MenuFlag::Normal,
+        s,
+        Message::Info_ManualeUtente,
+    );
+    menu_bar.add_emit(
+        "Info/Tutorial\t",
+        Shortcut::None,
+        menu::MenuFlag::Normal,
+        s,
+        Message::Info_Tutorial,
+    );
+    menu_bar.add_emit(
+        "Info/Supporto\t",
+        Shortcut::None,
+        menu::MenuFlag::Normal,
+        s,
+        Message::Info_Supporto,
+    );
+    menu_bar.end();
+}
+
+
+pub fn handle_menu_messages(msg:Message     ){
+
+    match msg {
+        Message::File_CorpusNuovo=> { // Call your function here
+            println!("File > Corpus Nuovo");
+        },
+        Message::File_CorpusEsistente=> { },
+        Message::File_ScanParseAlfabeto=> { },
+        Message::File_ScanParseSeparator=> { },
+        Message::File_Importa_MieRisorse=> { },
+        Message::File_Importa_TabellaInDataset=> { },
+        Message::File_Importa_ListaSempliceInDataset=> { },
+        Message::File_Esporta_DocumentiTipi=> { },
+        Message::File_Esporta_TipiVariabili=> { },
+        Message::File_Esporta_RiconstruzioneCorpus=> { },
+        Message::File_Esporta_Subcorpus=> { },
+        Message::File_Esci=> { },
+        Message::Analisi_Pretratamento=> { },
+        Message::Analisi_Lessicale_Tagging=> { },
+        Message::Analisi_Lessicale_EstrazioneDiParoleChiave=> { },
+        Message::Analisi_Testuale=> { },
+        Message::Impostationi_ModalitaCore_MonoCore=> { },
+        Message::Impostationi_ModalitaCore_MultiCore=> { },
+        Message::Impostationi_ModalitaSchermo=> { },
+        Message::Info_SuTalTac4=> { },
+        Message::Info_Licenza=> { },
+        Message::Info_ManualeUtente=> { },
+        Message::Info_Tutorial=> { },
+        Message::Info_Supporto=> {}
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -8,289 +8,142 @@ use fltk::{
     prelude::{GroupExt, InputExt, MenuExt, WidgetBase, WidgetExt, WindowExt},
     window::Window,
 };
-use fltk::{enums::*, frame::Frame, group::*, menu::*, prelude::*, window::*};
-use fltk::{prelude::*, *};
-
 use fltk::{
     dialog,
     enums::{Event, FrameType, Key, Shortcut},
     image, menu,
 };
+use fltk::{enums::*, frame::Frame, group::*, menu::*, prelude::*, window::*};
+use fltk::{prelude::*, *};
 use fltk_theme::{color_themes, ColorTheme};
 use fltk_theme::{widget_themes, ThemeType, WidgetTheme};
-pub mod utility;
-use crate::utility::Message;
+pub mod appmenu;
+use crate::appmenu::Message;
 
+//WINDOW PARAMETERS
 const WIN_WIDTH: i32 = 1000;
 const WIN_HEIGHT: i32 = 900;
 const LEFT_WIDTH: i32 = 300;
 const MENU_HEIGHT: i32 = 30;
-
 const RIGHT_WIDTH: i32 = WIN_WIDTH - LEFT_WIDTH;
 
-fn draw_menu(s: app::Sender<Message>) {
-  
 
+fn main() {
+ 
+    //build app and apply theme
+    let app = app::App::default();
+    let theme = ColorTheme::new(color_themes::GRAY_THEME);
+    theme.apply();
+    let widget_theme = WidgetTheme::new(ThemeType::Aero);
+    widget_theme.apply();
+    app::set_font_size(12);
 
-    let mut menu_bar = MenuBar::new(0, 0, WIN_WIDTH, 30, None);
-    menu_bar.add_emit(
-        "&File/Corpus Nuovo\t",
-        Shortcut::None,
-        menu::MenuFlag::Normal,
-        s,
-        Message::File_CorpusNuovo,
-    );
-    menu_bar.add_emit(
-        "&File/Corpus esistente\t",
-        Shortcut::None,
-        menu::MenuFlag::Normal,
-        s,
-        Message::File_CorpusEsistente,
-    );    menu_bar.add_emit(
-        "&File/Scan + Parse Alfabeto\t",
-        Shortcut::None,
-        menu::MenuFlag::Normal,
-        s,
-        Message::File_ScanParseAlfabeto,
-    );    menu_bar.add_emit(
-        "&File/Scan + Parse Separatori\t",
-        Shortcut::None,
-        menu::MenuFlag::Normal,
-        s,
-        Message::File_ScanParseSeparator,
-    );
+    //create window
+    let mut window = Window::default()
+        .with_size(WIN_WIDTH, WIN_HEIGHT)
+        .with_label("TaLTac 4")
+        .center_screen();
 
-    menu_bar.add_emit(
-        "&File/Importa/Mie risorse\t",
-        Shortcut::None,
-        menu::MenuFlag::Normal,
-        s,
-        Message::File_Importa_MieRisorse,
-    );
-    menu_bar.add_emit(
-        "&File/Importa/Tabella in Dataset\t",
-        Shortcut::None,
-        menu::MenuFlag::Normal,
-        s,
-        Message::File_Importa_TabellaInDataset,
-    );
+    //create menubar event handler
+    let (s, r) = app::channel::<Message>();
 
-    menu_bar.add_emit(
-        "&File/Importa/Lista semplice in Dataset\t",
-        Shortcut::None,
-        menu::MenuFlag::Normal,
-        s,
-        Message::File_Importa_ListaSempliceInDataset,
-    );
-    menu_bar.add_emit(
-        "&File/Esporta/Documenti - tipi",
-        Shortcut::None,
-        menu::MenuFlag::Normal,
-        s,
-        Message::File_Esporta_DocumentiTipi,
-    );
+    //build window contents
+    appmenu::build_menu(s, WIN_WIDTH);
+    build_content();
 
-    menu_bar.add_emit(
-        "&File/Esporta/Tipi - Variabili",
-        Shortcut::None,
-        menu::MenuFlag::Normal,
-        s,
-        Message::File_Esporta_TipiVariabili,
-    );
+    window.make_resizable(true);
+    window.end();
+    window.show();
 
-    menu_bar.add_emit(
-        "&File/Esporta/Riconstruzione Corpus",
-        Shortcut::None,
-        menu::MenuFlag::Normal,
-        s,
-        Message::File_Esporta_RiconstruzioneCorpus,
-    );
-
-    menu_bar.add_emit(
-        "&File/Esporta/Subcorpus",
-        Shortcut::None,
-        menu::MenuFlag::Normal,
-        s,
-        Message::File_Esporta_Subcorpus,
-    );
-
-    menu_bar.add_emit(
-        "&File/Esci\t",
-        Shortcut::None,
-        menu::MenuFlag::Normal,
-        s,
-        Message::File_Esci,
-    );
-    menu_bar.add_emit(
-        "Analisi/Pre-Tratamento\t",
-        Shortcut::None,
-        menu::MenuFlag::Normal,
-        s,
-        Message::Analisi_Pretratamento,
-    );
-    menu_bar.add_emit(
-        "Analisi/Lessicale/Tagging\t",
-        Shortcut::None,
-        menu::MenuFlag::Normal,
-        s,
-        Message::Analisi_Lessicale_Tagging,
-    );
-    menu_bar.add_emit(
-        "Analisi/Lessicale/Estrazione di Parole Chiave\t",
-        Shortcut::None,
-        menu::MenuFlag::Normal,
-        s,
-        Message::Analisi_Lessicale_EstrazioneDiParoleChiave,
-    );
-    menu_bar.add_emit(
-        "Analisi/Testuale\t",
-        Shortcut::None,
-        menu::MenuFlag::Normal,
-        s,
-        Message::Analisi_Testuale,
-    );
-
-    menu_bar.add_emit(
-        "Impostationi/Modalità Core/Mono-Core\t",
-        Shortcut::None,
-        menu::MenuFlag::Normal,
-        s,
-        Message::Impostationi_ModalitaCore_MonoCore,
-    );
-    menu_bar.add_emit(
-        "Impostationi/Modalità Core/Multi-Core\t",
-        Shortcut::None,
-        menu::MenuFlag::Normal,
-        s,
-        Message::Impostationi_ModalitaCore_MultiCore,
-    );
-    menu_bar.add_emit(
-        "Impostationi/Modalità Schermo\t",
-        Shortcut::None,
-        menu::MenuFlag::Normal,
-        s,
-        Message::Impostationi_ModalitaSchermo,
-    );
-    menu_bar.add_emit(
-        "Info/Su TalTac4\t",
-        Shortcut::None,
-        menu::MenuFlag::Normal,
-        s,
-        Message::Info_SuTalTac4,
-    );
-    menu_bar.add_emit(
-        "Info/Licenza\t",
-        Shortcut::None,
-        menu::MenuFlag::Normal,
-        s,
-        Message::Info_Licenza,
-    );
-    menu_bar.add_emit(
-        "Info/Manuale Utente\t",
-        Shortcut::None,
-        menu::MenuFlag::Normal,
-        s,
-        Message::Info_ManualeUtente,
-    );
-    menu_bar.add_emit(
-        "Info/Tutorial\t",
-        Shortcut::None,
-        menu::MenuFlag::Normal,
-        s,
-        Message::Info_Tutorial,
-    );
-    menu_bar.add_emit(
-        "Info/Supporto\t",
-        Shortcut::None,
-        menu::MenuFlag::Normal,
-        s,
-        Message::Info_Supporto,
-    );
-    menu_bar.end();
+    //listen to menubar events
+    while app.wait() {
+        if let Some(msg) = r.recv() {
+            appmenu::handle_menu_messages(msg);
+        }
+    }
 }
-fn draw_content() {
+
+
+
+fn build_content() {
     // Packed widget to hold the containers side by side
     let mut pack = Pack::new(0, 40, LEFT_WIDTH, 260, "");
     pack.set_type(PackType::Horizontal);
     pack.set_spacing(0); // Adjust spacing between the containers if needed
 
-    draw_left_panel();
+    build_left_panel();
     // Second container
     let _frame2 = Frame::new(LEFT_WIDTH, 200, RIGHT_WIDTH, 260, "Right Container");
-
     pack.end();
 }
 
-fn draw_left_panel() {
-    let mut leftpack = Pack::new(0, 40, LEFT_WIDTH, 260, "");
-    leftpack.set_type(PackType::Vertical);
-    leftpack.set_spacing(0); // Adjust spacing between the containers if needed
+fn build_left_panel() {
+    let mut pack = Pack::new(0, 40, LEFT_WIDTH, 260, "");
+    pack.set_type(PackType::Vertical);
+    pack.set_spacing(0); // Adjust spacing between the containers if needed
 
     draw_left_top_panel();
 
-    draw_left_middle_panel();
+    build_left_middle_panel();
 
-    draw_left_bottom_panel();
+    build_left_bottom_panel();
 
-    leftpack.end();
+    pack.end();
 }
-fn draw_left_bottom_panel() {
-    let mut leftpack = Pack::new(0, 40, LEFT_WIDTH, 260, "");
-    leftpack.set_type(PackType::Vertical);
-    leftpack.set_spacing(0); // Adjust spacing between the containers if needed
-    draw_left_bottom_panel_content();
+fn build_left_bottom_panel() {
+    let mut pack = Pack::new(0, 40, LEFT_WIDTH, 260, "");
+    pack.set_type(PackType::Vertical);
+    pack.set_spacing(0); // Adjust spacing between the containers if needed
+    build_left_bottom_panel_content();
     // Set the text size
-   leftpack.end();
+    pack.end();
 }
-fn draw_left_bottom_panel_content(){
-    drawLeftText("Dati");
-    drawLeftText("Nome: Sessione del None");
-    drawLeftText("File: VOC (|type|, Sum occ): (?,?)");
-    drawLeftText("Multi-core: OFF");
- 
- }
-fn drawLeftText(text:&str){
-    let _frame3 = Frame::new(0, 400, LEFT_WIDTH, 30, text).set_align(enums::Align::Left | enums::Align::Inside );
-    
+fn build_left_bottom_panel_content() {
+    draw_text_aligned_left("Dati");
+    draw_text_aligned_left("Nome: Sessione del None");
+    draw_text_aligned_left("File: VOC (|type|, Sum occ): (?,?)");
+    draw_text_aligned_left("Multi-core: OFF");
 }
-fn draw_left_middle_panel() {
+fn draw_text_aligned_left(text: &str) {
+    let _frame3 = Frame::new(0, 400, LEFT_WIDTH, 30, text)
+        .set_align(enums::Align::Left | enums::Align::Inside);
+}
+fn build_left_middle_panel() {
     let mut tabs2 = Tabs::new(0, MENU_HEIGHT, LEFT_WIDTH, 400, "");
-    //let mut tab = Tabs::default_fill();
-    let mut grp1 = Flex::default_fill().with_label("Corpus\t\t").row();
+    let mut flex1 = Flex::default_fill().with_label("Corpus\t\t").row();
     let mut col = Flex::default().column();
-    grp1.fixed(&col, 160);
+    flex1.fixed(&col, 160);
     col.set_pad(10);
     col.set_margin(10);
 
-        //let _frame1 = Frame::new(0, 40, 200, 80, "Top Container");
-        let mut frame2_pack = Pack::new(0, 40, LEFT_WIDTH, 260, "");
-        frame2_pack.set_type(fltk::group::PackType::Vertical);
-        frame2_pack.set_spacing(10); // Sets the spacing between widgets
+    //let _frame1 = Frame::new(0, 40, 200, 80, "Top Container");
+    let mut frame2_pack = Pack::new(0, 40, LEFT_WIDTH, 260, "");
+    frame2_pack.set_type(fltk::group::PackType::Vertical);
+    frame2_pack.set_spacing(10); // Sets the spacing between widgets
 
-        frame2_pack.end();
+    frame2_pack.end();
 
     col.end();
-    grp1.end();
+    flex1.end();
 
-    let grp2 = Flex::default_fill().with_label("Datasets\t\t").row();
-    grp2.end();
+    let flex2 = Flex::default_fill().with_label("Datasets\t\t").row();
+    flex2.end();
 
     tabs2.end();
     tabs2.auto_layout();
 }
 fn draw_left_top_panel() {
     let mut tabs = Tabs::new(0, MENU_HEIGHT, LEFT_WIDTH, 300, "");
-    //let mut tab = Tabs::default_fill();
-    let mut grp1 = Flex::default_fill().with_label("Sessioni\t\t").row();
+    let mut flex = Flex::default_fill().with_label("Sessioni\t\t").row();
     let mut col = Flex::default().column();
-    grp1.fixed(&col, LEFT_WIDTH);
-        col.set_pad(10);
-        col.set_margin(10);
+    flex.fixed(&col, LEFT_WIDTH);
+    col.set_pad(10);
+    col.set_margin(10);
 
-      draw_left_top_panel_tab1();
+    draw_left_top_panel_tab1();
 
-        col.end();
-    grp1.end();
+    col.end();
+    flex.end();
 
     let grp2 = Flex::default_fill().with_label("Risorsi\t\t").row();
     grp2.end();
@@ -302,77 +155,16 @@ fn draw_left_top_panel() {
     tabs.auto_layout();
 }
 
+fn draw_left_top_panel_tab1() {
+    //let _frame1 = Frame::new(0, 40, 200, 80, "Top Container");
+    let mut pack = Pack::new(0, 40, LEFT_WIDTH, 260, "");
+    pack.set_type(fltk::group::PackType::Vertical);
+    pack.set_spacing(10); // Sets the spacing between widgets
 
-fn draw_left_top_panel_tab1(){
-      //let _frame1 = Frame::new(0, 40, 200, 80, "Top Container");
-      let mut frame1_pack = Pack::new(0, 40, LEFT_WIDTH, 260, "");
-      frame1_pack.set_type(fltk::group::PackType::Vertical);
-      frame1_pack.set_spacing(10); // Sets the spacing between widgets
-
-      let mut btn1 = button::Button::new(0, 0, LEFT_WIDTH, 40, "Crea une Nuova Sessione");
-      let mut btn2 = button::Button::new(0, 0, LEFT_WIDTH, 40, "Cancella sessione");
-      drawLeftText("Sessione:");
-      let mut choice = menu::Choice::new(50, 100, LEFT_WIDTH, 30, None);
-      choice.add_choice("Session: 05-03-2024 10:35 | 06-03-2024 14:22 ");
-      frame1_pack.end();
-
-}
-
-fn main() {
-    let (s, r) = app::channel::<Message>();
-
-    
-    let app = app::App::default();
-    let theme = ColorTheme::new(color_themes::GRAY_THEME);
-    theme.apply();
-    let widget_theme = WidgetTheme::new(ThemeType::Aero);
-    widget_theme.apply();
-    app::set_font_size(12);
-    let mut wind = Window::default()
-        .with_size(WIN_WIDTH, WIN_HEIGHT)
-        .with_label("TaLTac 4")
-        .center_screen();
-
-    draw_menu(s);
-    draw_content();
-
-    wind.make_resizable(true);
-    wind.end();
-    wind.show();
-
-//    app.run().unwrap();
-
-    while app.wait() {
-        if let Some(msg) = r.recv() {
-            match msg {
-                Message::File_CorpusNuovo=> { // Call your function here
-                    println!("File > Corpus Nuovo");
-                },
-                Message::File_CorpusEsistente=> { },
-                Message::File_ScanParseAlfabeto=> { },
-                Message::File_ScanParseSeparator=> { },
-                Message::File_Importa_MieRisorse=> { },
-                Message::File_Importa_TabellaInDataset=> { },
-                Message::File_Importa_ListaSempliceInDataset=> { },
-                Message::File_Esporta_DocumentiTipi=> { },
-                Message::File_Esporta_TipiVariabili=> { },
-                Message::File_Esporta_RiconstruzioneCorpus=> { },
-                Message::File_Esporta_Subcorpus=> { },
-                Message::File_Esci=> { },
-                Message::Analisi_Pretratamento=> { },
-                Message::Analisi_Lessicale_Tagging=> { },
-                Message::Analisi_Lessicale_EstrazioneDiParoleChiave=> { },
-                Message::Analisi_Testuale=> { },
-                Message::Impostationi_ModalitaCore_MonoCore=> { },
-                Message::Impostationi_ModalitaCore_MultiCore=> { },
-                Message::Impostationi_ModalitaSchermo=> { },
-                Message::Info_SuTalTac4=> { },
-                Message::Info_Licenza=> { },
-                Message::Info_ManualeUtente=> { },
-                Message::Info_Tutorial=> { },
-                Message::Info_Supporto=> {}
-            }
-        }
-    }
-
+    let mut btn1 = button::Button::new(0, 0, LEFT_WIDTH, 40, "Crea une Nuova Sessione");
+    let mut btn2 = button::Button::new(0, 0, LEFT_WIDTH, 40, "Cancella sessione");
+    draw_text_aligned_left("Sessione:");
+    let mut choice = menu::Choice::new(50, 100, LEFT_WIDTH, 30, None);
+    choice.add_choice("Session: 05-03-2024 10:35 | 06-03-2024 14:22 ");
+    pack.end();
 }
